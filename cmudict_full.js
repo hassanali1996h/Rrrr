bb6989b9d53aea97023ec3c5ae6428e50a6945a4
hassanali1996h <HUGNVF@GMAIL.COM>
2025-06-26 15:03:34 +0300

    Delete cmudict_full.js

--- cmudict_full.js
+++
@@ -1,6 +0,0 @@
-// CMU Pronouncing Dictionary - placeholder
-// Add the full CMU dict object below:
-window.cmuDict = {
-  // "hello": ["HH","AH0","L","OW1"],
-  // ... full dictionary entries ...
-};
